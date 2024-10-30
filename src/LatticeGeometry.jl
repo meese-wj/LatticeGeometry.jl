7b3d8303ee457c0cb8840c47b19f0d10ecf8454e
W. Joe Meese <meese022@umn.edu>
2024-10-29 21:14:57 -0500

    Added an AbstractionError to prevent silly mistakes

--- src/LatticeGeometry.jl
+++ src/LatticeGeometry.jl
@@ -1,5 +1,22 @@
 module LatticeGeometry
 
-# Write your package code here.
+import LinearAlgebra
+import StaticArrays
+
+abstract type AbstractUnitCell end
+names(cell::AbstractUnitCell) = throw(AbstractionError(names, cell))
+num_atoms(cell::AbstractUnitCell) = length(names(cell))
+
+
+struct UnitCell{D,T} <: AbstractUnitCell
+    atom_names::StaticArrays.SVector{String}
+    atom_positions::StaticArrays.SVector{D, T}
+
+end
+
+abstract type AbstractLattice end
+struct Lattice <: AbstractLattice
+
+end
 
 end
---
+++ src/util.jl
@@ -0,0 +1,30 @@
+
+import Base: Exception, showerror
+export showerror
+
+"""
+    AbstractionError <: Exception
+
+A pretty generic catch all for abtract typing on the fly.
+"""
+struct AbstractionError <: Base.Exception 
+    func::Function
+    args::Vector{Type}
+
+    function AbstractionError(func, args...)
+        arg_vec = map( a -> typeof(a), [args...] )
+        return new(func, arg_vec)
+    end
+end
+function message(err::AbstractionError)
+    signature = "($(err.args[begin])"
+    for arg ∈ err.args[begin+1:end-1]
+        signature *= ", $(arg)"
+    end
+    if length(err.args) ≥ 2
+        signature *= ", $(err.args[end]))"
+    end
+    return "The function `$(err.func)` has no method definition with the signature: `$(signature)`."
+end
+
+Base.showerror(io::IO, err::AbstractionError) = print(io, "AbstractionError: $(message(err))")
